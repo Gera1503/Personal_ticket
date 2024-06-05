d8b7354e08155c0ff8e2ae849ae459bbb5321e17
unknown <gerardorincon1503@gmail.com>
2024-06-05 15:37:27 -0600

    05/06/2024 mtperfil editado

--- view/mntperfil/mntperfil.js
+++ view/mntperfil/mntperfil.js
@@ -9,13 +9,13 @@ $(document).on("click", "#btnactualizar",function(){
     if(pass==newpass){
         var usu_id=$('#user_idx').val();
         $.post("../../controller/usuario.php?op=password", {usu_id : usu_id,usu_pass:newpass} ,function(data){
-            swal("correcto","Actualizado Correctamente", "success");
+            swal("correcto","Se actualizo Correctamente", "success");
         });
 
     }else{
         swal("Error!","No coinciden las contraseñas","error");
     }
 
-   }
+   } 
 
 });
